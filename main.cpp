f9febe0bc4bbc509c053f305ce98386eb643fbb2
ZSergeyN <zsergeyn@outlook.com>
2025-02-20 01:27:36 +0600

    Added management of requests.json files. Added the function of launching the search engine

--- main.cpp
+++ main.cpp
@@ -8,12 +8,23 @@ int main(int argc, char *argv[])
     controlPanelMainWindow window(nullptr);
     Ui::MainWindow controlPanel;
     controlPanel.setupUi(&window);
+
     window.name = controlPanel.name;
     window.version = controlPanel.version;
     window.maxResponses = controlPanel.maxResponses;
     window.listForFiles = controlPanel.listForFiles;
     window.saveConfigButton = controlPanel.saveConfigButton;
     window.loadListFilesButton = controlPanel.loadListFilesButton;
+
+    window.textRequest = controlPanel.textRequest;
+    window.listRequest = controlPanel.listRequest;
+    window.requestsLoadButton = controlPanel.requestsLoadButton;
+    window.saveRequestsButton = controlPanel.saveRequestsButton;
+    window.requestsClearButton = controlPanel.requestsClearButton;
+
+    window.statusFile = controlPanel.statusFile;
+    window.listResult = controlPanel.listResult;
+
     window.setFixedSize(502, 680);
     window.show();
     return QApplication::exec();
